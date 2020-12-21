86d9b5ba70c7e993cc6d5a0c7f3975a36500ff19
kzawora <konrad.zawora@outlook.com>
2020-12-21 17:55:29 +0100

    Add request queue

--- app/src/main/java/com/lsorter/capture/RemoteLegoBrickImagesCapture.kt
+++ app/src/main/java/com/lsorter/capture/RemoteLegoBrickImagesCapture.kt
@@ -1,6 +1,7 @@
 package com.lsorter.capture;
 
 import android.annotation.SuppressLint
+import android.media.MediaActionSound
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.internal.utils.ImageUtil
@@ -13,6 +14,7 @@ import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class RemoteLegoBrickImagesCapture : LegoBrickDatasetCapture {
 
@@ -20,23 +22,25 @@ class RemoteLegoBrickImagesCapture : LegoBrickDatasetCapture {
     private val connectionChannel = connectionManager.getConnectionChannel()
     private val cameraExecutor: Executor = Executors.newSingleThreadExecutor()
     private var scope = MainScope()
+    private val requestScope = CoroutineScope(Dispatchers.Default)
+    private var requestQueue = ConcurrentLinkedQueue<LegoBrickProto.ImageStore>()
 
     private val legoBrickService: LegoBrickGrpc.LegoBrickBlockingStub =
-        LegoBrickGrpc.newBlockingStub(connectionChannel)
+            LegoBrickGrpc.newBlockingStub(connectionChannel)
 
     @SuppressLint("RestrictedApi", "CheckResult")
     fun sendLegoImageWithLabel(image: ImageProxy, label: String) {
         val request = LegoBrickProto.ImageStore.newBuilder()
-            .setImage(
-                ByteString.copyFrom(
-                    ImageUtil.imageToJpegByteArray(image)
+                .setImage(
+                        ByteString.copyFrom(
+                                ImageUtil.imageToJpegByteArray(image)
+                        )
                 )
-            )
-            .setRotation(image.imageInfo.rotationDegrees)
-            .setLabel(label)
-            .build()
-
-        legoBrickService.collectCroppedImages(request)
+                .setRotation(image.imageInfo.rotationDegrees)
+                .setLabel(label)
+                .build()
+        requestQueue.add(request)
+//        legoBrickService.collectCroppedImages(request)
     }
 
     override fun captureImages(imageCapture: ImageCapture, frequencyMs: Int, label: String) {
@@ -45,25 +49,38 @@ class RemoteLegoBrickImagesCapture : LegoBrickDatasetCapture {
             while (true) {
                 if (canProcessNext.get()) {
                     canProcessNext.set(false)
+                    val sound = MediaActionSound()
+                    sound.play(MediaActionSound.SHUTTER_CLICK)
                     imageCapture.takePicture(cameraExecutor,
-                        object : ImageCapture.OnImageCapturedCallback() {
-                            override fun onCaptureSuccess(image: ImageProxy) {
-                                sendLegoImageWithLabel(image, label)
-                                image.close()
-                                canProcessNext.set(true)
-                            }
-                        })
+                            object : ImageCapture.OnImageCapturedCallback() {
+                                override fun onCaptureSuccess(image: ImageProxy) {
+                                    sendLegoImageWithLabel(image, label)
+                                    image.close()
+                                    canProcessNext.set(true)
+                                }
+                            })
                 }
-
                 delay(frequencyMs.toLong())
+            }
+        }
+        requestScope.launch {
+            while (true) {
+                if (requestQueue.isEmpty()) continue
+                var request = requestQueue.poll()
+                legoBrickService.collectCroppedImages(request)
             }
         }
     }
 
     override fun stop() {
-        connectionChannel.shutdown()
-        connectionChannel.awaitTermination(1000, TimeUnit.MILLISECONDS)
+        requestScope.cancel()
         scope.cancel()
         scope = MainScope()
+        while (!requestQueue.isEmpty()) {
+            var request = requestQueue.poll()
+            legoBrickService.collectCroppedImages(request)
+        }
+        connectionChannel.shutdown()
+        connectionChannel.awaitTermination(1000, TimeUnit.MILLISECONDS)
     }
 }
